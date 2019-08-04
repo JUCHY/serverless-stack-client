3a4bd8460e988d6c9ef29fcde7049a6515e0ac5d
Joshua Tagoe <34180299+JUCHY@users.noreply.github.com>
2019-08-04 18:18:00 -0400

    configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
+const dev = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1r7tniak83eff"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://92j1o3llh8.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_AMgVWhmlt",
+    APP_CLIENT_ID: "5u4q1bd8i7pkce5dutidrf8qkf",
+    IDENTITY_POOL_ID: "us-east-1:888e23b5-d19b-4756-ada6-99e68bbc9e6b"
+  }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-mwqyyx3ss3ue"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://nn6kqce40m.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_X1jis9WVR",
+    APP_CLIENT_ID: "6b15ujef5ive73nv1jn7n5reqq",
+    IDENTITY_POOL_ID: "us-east-1:03d912ab-deff-4a73-afcb-510e70a77bc7"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "notes-apps-upload-test"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://x0c4f4r1cf.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_JK5x49CRH",
-      APP_CLIENT_ID: "4v1svp7orke4uqps22d0ildpbs",
-      IDENTITY_POOL_ID: "us-east-1:3b73168d-dcbe-4641-9007-11c417ff51ae"
-    }
-  };
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
